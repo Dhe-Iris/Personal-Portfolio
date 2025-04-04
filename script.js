d97ef5bd1eaa6d8903125760aa8bff8ffe2e8ed0
Dhe-Iris <dolapog44@gmail.com>
2025-04-04 03:24:22 +0100

    all files added

---
+++ script.js
@@ -0,0 +1,42 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+// -----sidebar----
+let sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+// --------form to google sheets---------
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxQY0qT6RfJRv697OXQUlQA5lnAWJzmgt7hdm3NfXna_4BGQZajkpCncIQNnFo4S0DAUQ/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message has been successfully sent!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
